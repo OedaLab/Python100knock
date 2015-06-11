795719e56ae91b3ff29196c5da1a86dd2ffa2612
Shinichi Oeda <crotsu@gmail.com>
2015-06-11 23:37:46 +0900

    問9を追加．文字列の単語の数をカウント

---
+++ countWords.py
@@ -0,0 +1,14 @@
+str = 'Could you send me an e-mail.'
+print(str)
+
+# ダサい
+ct = 0
+for i in str:
+    if i==' ':
+        ct+=1
+ct += 1
+print('word1 = %d' %(ct))
+
+# クール
+ct = len(str.split())
+print('word2 = %d' %(ct))
